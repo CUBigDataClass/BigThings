0e044d2c7eec13a8f21b99030c37268d08d45433
Nick Isaacs <nisaacs@splickit.com>
2014-02-23 18:26:23 -0700

    twitter initializer

--- config/initializers/twitter.js
+++ config/initializers/twitter.js
@@ -1,3 +1,28 @@
-var fs = require('fs')
-var twitter = require('twitter')
+var util = require('util'),
+twitter = require('twitter'),
+fs = require('fs');
+    
 
+var twitter = function(callback){
+  if(!this.client){
+    fs.readFile(__dirname + "/../twitter.json", "utf-8", function(err, data){
+      if (err){
+        throw err
+      } else{
+        var config = JSON.parse(data)[(process.env.NODE_ENV || 'development')]
+      
+        this.client = new twitter({
+          consumer_key: config["api_key"],
+          consumer_secret: config["api_secret"],
+          access_token_key: config["access_token"],
+          access_token_secret: config["access_token_secret"]
+        })
+        callback(this.client)
+      }
+    })
+  } else{
+    callback(this.client)
+  }
+}
+
+module.exports = twitter
